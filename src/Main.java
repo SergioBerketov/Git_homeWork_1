82b8041404e2055eef56076f5223d36370eee163
Sergei Berketov <sergio_berketov@mail.ru>
2023-01-13 16:05:42 +0700

    modified Main & added README

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SalesManager sm = new SalesManager(new int[]{3, 6, 9});
+        System.out.println(sm.max());
     }
 }
